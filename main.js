6ac64933c157c4224ae1ecc391f952a606f3ec22
gRoussac <gRoussac@users.noreply.github.com>
2022-10-10 17:32:45 +0200

    update send transac

--- main.js
+++ main.js
@@ -66,3 +66,61 @@ async function AccountInformation() {
     textBalance.textContent = `Balance ${balance.toString()}`;
   }
 }
+
+async function sendTransaction() {
+  // get address to send from input.
+  const to = document.getElementById('Recipient').value;
+  // get amount to send from input.
+  const amount = document.getElementById('Amount').value;
+  // For native-transfers the payment price is fixed.
+  const paymentAmount = 100000000;
+
+  // transfer_id field in the request to tag the transaction and to correlate it to your back-end storage.
+  const id = 287821;
+
+  // gasPrice for native transfers can be set to 1.
+  const gasPrice = 1;
+
+  // Time that the deploy will remain valid for, in milliseconds
+  // The default value is 1800000 ms (30 minutes).
+  const ttl = 1800000;
+  const publicKeyHex = await window.casperlabsHelper.getActivePublicKey();
+  const publicKey = CLPublicKey.fromHex(publicKeyHex);
+
+  let deployParams = new DeployUtil.DeployParams(
+    publicKey,
+    'casper-net-1',
+    gasPrice,
+    ttl
+  );
+
+  // We create a public key from account-address (it is the hex representation of the public-key with an added prefix).
+  const toPublicKey = CLPublicKey.fromHex(to);
+
+  const session = DeployUtil.ExecutableDeployItem.newTransfer(
+    amount,
+    toPublicKey,
+    null,
+    id
+  );
+
+  const payment = DeployUtil.standardPayment(paymentAmount);
+  const deploy = DeployUtil.makeDeploy(deployParams, session, payment);
+
+  // Turn your transaction data to format JSON
+  const json = DeployUtil.deployToJson(deploy);
+
+  // Sign transcation using casper-signer.
+  const signature = await window.casperlabsHelper.sign(json, publicKeyHex, to);
+  const deployObject = DeployUtil.deployFromJson(signature);
+
+  // Here we are sending the signed deploy.
+  const signed = await casperClient.putDeploy(deployObject.val);
+
+  // Display transaction address
+  const tx = document.getElementById('tx');
+  tx.textContent = `tx: ${signed}`;
+}
+
+const btnSend = document.getElementById('btnSend');
+btnSend.addEventListener('click', async () => await sendTransaction());
